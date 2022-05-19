7b4b82104a19e426c67dade98ba0c20627f5da9b
NSACloud <46909075+NSACloud@users.noreply.github.com>
2022-05-18 22:58:08 -0400

    Initial Release

---
+++ __init__.py
@@ -0,0 +1,181 @@
+#Author: NSA Cloud
+bl_info = {
+    "name": "RE Mesh Noesis Wrapper",
+    "author": "NSA Cloud",
+    "version": (1, 0),
+    "blender": (2, 93, 0),
+    "location": "File > Import-Export",
+    "description": "Import and export RE Engine Mesh files using Noesis.",
+    "warning": "",
+    "wiki_url": "https://github.com/NSACloud/Blender-RE-Mesh-Noesis-Wrapper",
+    "tracker_url": "",
+    "category": "Import-Export"}
+
+import bpy
+import os
+
+from bpy_extras.io_utils import ExportHelper,ImportHelper
+from bpy.props import StringProperty, BoolProperty, CollectionProperty
+from bpy.types import Operator, OperatorFileListElement,AddonPreferences
+
+from .modules.file_re_mesh_noesis import importREMeshFileNoesis,exportREMeshFileNoesis
+
+os.system("color")#Enable console colors
+
+def showMessageBox(message = "", title = "Message Box", icon = 'INFO'):
+
+    def draw(self, context):
+        self.layout.label(text = message)
+
+    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+
+class REMeshNoesisWrapperPreferences(AddonPreferences):
+    bl_idname = __name__
+ 
+    noesisPath: StringProperty(
+        name="Noesis.exe Path",
+        subtype='FILE_PATH',
+    )
+ 
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "noesisPath")
+
+class ImportREMeshNoesis(Operator, ImportHelper):
+    '''Import RE Mesh File'''
+    bl_idname = "re_mesh_noesis.importfile"
+    bl_label = "Import RE Mesh"
+    bl_options = {'PRESET', "REGISTER", "UNDO"}
+    files : CollectionProperty(
+            name="File Path",
+            type=OperatorFileListElement,
+            )
+    directory : StringProperty(
+            subtype='DIR_PATH',
+            )
+    filename_ext = ".mesh.*"
+    filter_glob: StringProperty(default="*.mesh.*", options={'HIDDEN'})
+    clear_scene : BoolProperty(
+       name = "Clear scene before import.",
+       description = "Clears all objects before importing",
+       default = True)
+    def execute(self, context):
+        options = {"clearScene":self.clear_scene}
+        if os.path.isfile(bpy.context.preferences.addons[__name__].preferences.noesisPath.replace("\"","")):#Strip quotation marks if they're in the noesis path
+            directory = self.directory
+            for file_elem in self.files:
+                filepath = os.path.join(directory, file_elem.name)
+                if os.path.isfile(filepath):
+                    result = importREMeshFileNoesis(filepath,bpy.context.preferences.addons[__name__].preferences.noesisPath.replace("\"",""),options)#Returns tuple containing list of warnings and errors
+                    if result[1] != []:#Error List
+                        showMessageBox("An error occurred during import. See Window > Toggle System Console for details.","Error","ERROR")
+                        for error in result[1]:
+                            print('\033[91m'+error+'\033[0m')
+                            self.report({"INFO"},"Failed to import RE Mesh.")
+                        for warning in result[0]:
+                            print('\033[93m'+warning+'\033[0m')
+                        return {'CANCELLED'}
+                    elif result[0] != []:#Warning List
+                        showMessageBox("Warnings occurred during import. See Window > Toggle System Console for details.","Warning","ERROR")
+                        for warning in result[0]:
+                            print('\033[93m'+warning+'\033[0m')
+                    self.report({"INFO"},"Imported RE Mesh successfully.")
+                    return {'FINISHED'}
+        else:
+            msg = "Error: Noesis path is not set or is invalid. Set the path to Noesis.exe in Preferences > Addons > RE Mesh Noesis Wrapper."
+            showMessageBox(msg,"RE Mesh Import","ERROR")
+            print('\033[91m'+msg+'\033[0m')
+            self.report({"INFO"},"Noesis.exe path is not set.")
+            return {'CANCELLED'}
+        
+class ExportREMeshNoesis(Operator, ExportHelper):
+    '''Export RE Mesh File'''
+    bl_idname = "re_mesh_noesis.exportfile"
+    bl_label = "Export RE Mesh"
+    bl_options = {'PRESET'}
+    filename_ext = ".2008058288"
+    filter_glob: StringProperty(default="*.mesh*", options={'HIDDEN'})
+    #FBX Exporter Settings
+    selection_only : BoolProperty(
+       name = "Export Selected Objects Only",
+       description = "Limit export to selected objects",
+       default = False)
+    #Noesis Exporter Arguments
+    rewrite : BoolProperty(
+       name = "Rewrite",
+       description = "Rewrite submeshes and materials structure. NOTE: If rewrite is disabled, the materials, mesh count and bones must match the original file",
+       default = True)
+    bonenumbers : BoolProperty(
+       name = "Bone Numbers",
+       description = "Add bone numbers to imported bones",
+       default = False)
+    flip : BoolProperty(
+       name = "Flip",
+       description = "Reverse handedness from DirectX to OpenGL",
+       default = False)
+    bones : BoolProperty(
+       name = "Bones",
+       description = "Write new skeleton on export. NOTE: Rewrite also writes new skeletons",
+       default = False)
+
+    def execute(self, context):
+        options = {"selection_only":self.selection_only,"rewrite":self.rewrite,"bonenumbers":self.bonenumbers,"flip":self.flip,"bones":self.bones}
+        
+        if os.path.isfile(bpy.context.preferences.addons[__name__].preferences.noesisPath.replace("\"","")):
+            
+            result = exportREMeshFileNoesis(self.filepath,bpy.context.preferences.addons[__name__].preferences.noesisPath.replace("\"",""),options)
+            report = "Exported RE Mesh successfully."
+            if result[1] != []:#Error List
+                showMessageBox("An error occurred during export. See Window > Toggle System Console for details.","Error","ERROR")
+                for error in result[1]:
+                    print('\033[91m'+error+'\033[0m')
+                for warning in result[0]:
+                    print('\033[93m'+warning+'\033[0m')
+                self.report({"INFO"},"Failed to export RE Mesh.")
+                return {'CANCELLED'}
+            elif result[0] != []:#Warning List
+                showMessageBox("Warnings occurred during export. See Window > Toggle System Console for details.","Warning","ERROR")
+                report = "Exported RE Mesh with warnings."
+                for warning in result[0]:
+                    print('\033[93m'+warning+'\033[0m')
+                
+            self.report({"INFO"},report)
+            return {"FINISHED"}
+        else:
+            msg = "Error: Noesis path is not set or is invalid. Set the path to Noesis.exe in Preferences > Addons > RE Mesh Noesis Wrapper."
+            showMessageBox(msg,"RE Mesh Import","ERROR")
+            print('\033[91m'+msg+'\033[0m')
+            self.report({"INFO"},"Noesis.exe path is not set.")
+            return {'CANCELLED'}
+
+# Registration
+classes = [
+    ImportREMeshNoesis,
+    ExportREMeshNoesis,
+    REMeshNoesisWrapperPreferences
+    ]
+
+
+def re_mesh_noesis_import(self, context):
+    self.layout.operator(ImportREMeshNoesis.bl_idname, text="RE Mesh (.mesh) (Noesis)")
+    
+def re_mesh_noesis_export(self, context):
+    self.layout.operator(ExportREMeshNoesis.bl_idname, text="RE Mesh (.mesh) (Noesis)")
+
+
+def register():
+    for classEntry in classes:
+        bpy.utils.register_class(classEntry)
+        
+    bpy.types.TOPBAR_MT_file_import.append(re_mesh_noesis_import)
+    bpy.types.TOPBAR_MT_file_export.append(re_mesh_noesis_export)
+    
+    
+def unregister():
+    for classEntry in classes:
+        bpy.utils.unregister_class(classEntry)
+        
+    bpy.types.TOPBAR_MT_file_import.remove(re_mesh_noesis_import)
+    bpy.types.TOPBAR_MT_file_export.remove(re_mesh_noesis_export)
+if __name__ == '__main__':
+    register()
---
+++ modules/file_re_mesh_noesis.py
@@ -0,0 +1,143 @@
+#Author: NSA Cloud
+import bpy
+
+import os
+import subprocess
+
+FBX_IMPORT_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"TempFBX","import")#RE-Mesh-Noesis-Wrapper\TempFBX\import
+FBX_EXPORT_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"TempFBX","export")#RE-Mesh-Noesis-Wrapper\TempFBX\export
+#---RE MESH IO FUNCTIONS---#
+
+def importREMeshFileNoesis(filePath,noesisPath,options):
+
+    fileName = os.path.split(filePath)[1].split(".mesh")[0]
+    warningList = []
+    errorList = []
+    try:
+        os.makedirs(FBX_IMPORT_DIR, exist_ok = True)
+    except OSError as error:
+        errorList.append("Error: Could not create FBX import directory. " + str(error))
+        return (warningList,errorList)
+    if options["clearScene"]:
+    	for collection in bpy.data.collections:
+    		for obj in collection.objects:
+    			collection.objects.unlink(obj)
+    		bpy.data.collections.remove(collection)
+    	for bpy_data_iter in (bpy.data.objects,bpy.data.meshes,bpy.data.lights,bpy.data.cameras):
+    		for id_data in bpy_data_iter:
+    			bpy_data_iter.remove(id_data)
+    	for material in bpy.data.materials:
+    		bpy.data.materials.remove(material)
+    	for amt in bpy.data.armatures:
+    		bpy.data.armatures.remove(amt)
+    	for obj in bpy.data.objects:
+    		bpy.data.objects.remove(obj)
+    		obj.user_clear()
+
+    print("\033[96m__________________________________\nRE Mesh import started.\033[0m")
+    noesisCommand = "\""+noesisPath + "\" ?cmode " +"\"" + filePath + "\" " + "\""+os.path.join(FBX_IMPORT_DIR,fileName+".fbx") +"\""
+    print("Running Noesis...")
+    #print(noesisCommand)
+    subprocess.run(noesisCommand,shell=True)
+    if os.path.exists(os.path.join(FBX_IMPORT_DIR,fileName+".fbx")):
+        try:
+            bpy.ops.import_scene.fbx(filepath = os.path.join(FBX_IMPORT_DIR,fileName+".fbx"))
+        except Exception as error:
+            errorList.append("ERROR: FBX import failed. Check that the Noesis path is correct and that the RE Mesh plugin is installed.\n" + str(error))
+            return (warningList,errorList)
+    else:
+        errorList.append("ERROR: No FBX file was created by Noesis. The mesh file may be corrupted or unreadable.")
+        return (warningList,errorList)
+    try:
+        os.remove(os.path.join(FBX_IMPORT_DIR,fileName+".fbx"))
+    except:
+        warningList.append("WARNING: Failed to delete temporary FBX from TempFBX directory.")
+    print("\033[92m__________________________________\nRE Mesh import finished.\033[0m")
+    return (warningList,errorList)
+    
+def exportREMeshFileNoesis(filePath,noesisPath,options):
+    
+    fileName = os.path.split(filePath)[1].split(".mesh")[0]
+    warningList = []
+    errorList = []
+    try:
+        os.makedirs(FBX_EXPORT_DIR, exist_ok = True)
+    except OSError as error:
+        errorList.append("Error: Could not create FBX export directory. " + str(error))
+        return (warningList,errorList)
+    print("\033[96m__________________________________\nRE Mesh export started.\033[0m")
+    
+    #Basic Error Checking
+    if options["selection_only"]:
+        selection = bpy.context.selected_objects
+    else:
+        selection = bpy.context.scene.objects
+    containsMesh = False
+    armatureCount = 0
+    for obj in selection:
+        if obj.type == "MESH":
+            containsMesh = True
+            #Check if mesh contains at least one face
+            if len(obj.data.loops) == 0:
+                errorList.append("ERROR: " + obj.name + " has no faces. A mesh requires at least one face.")
+            #Check if parented to armature
+            if len(obj.vertex_groups) > 0:
+                    
+                armature = None
+                for modifier in obj.modifiers:
+                    if modifier.type == "ARMATURE":
+                        #print(modifier.object)
+                        try:
+                            if bpy.data.objects.get(modifier.object.name):#Check if armature modifier points to an armature
+                                armature = modifier.object
+                        except:
+                            pass
+                if armature != None:
+                    #Check that every vertex group exists on the armature
+                    for vertexGroup in obj.vertex_groups:
+                        emptyVertexGroup = not any(vertexGroup.index in [g.group for g in v.groups] for v in obj.data.vertices)
+                        if armature.data.bones.get(vertexGroup.name,None) == None and not emptyVertexGroup:
+                            warningList.append("WARNING: " + obj.name + " is weighted to " + vertexGroup.name + ", but that bone does not exist on the armature." )
+                else:
+                    warningList.append("WARNING: " +obj.name + " contains vertex groups but is not parented to an armature.")
+                #Check that naming scheme is valid
+            split = obj.name.split("_")
+            try:
+                if split[0].split("_")[0] != "LODGroup" or split[2].split("_")[0] != "MainMesh" or split[4].split("_")[0] != "SubMesh" or split[6].split("_")[0] != "":
+                    warningList.append("WARNING: "+obj.name + " does not match the Noesis plugin mesh naming scheme:\n\t LODGroup_X_MainMesh_Y_SubMesh_Z__materialName")
+            except:
+                warningList.append("WARNING: "+obj.name + " does not match the Noesis plugin mesh naming scheme:\n\tLODGroup_X_MainMesh_Y_SubMesh_Z__materialName")
+        elif obj.type == "ARMATURE":
+            #Get amount of armatures in selection
+            armatureCount += 1
+    if armatureCount > 1:
+        warningList.append("WARNING: More than one armature is present. Only one armature can be used.")
+    if not containsMesh:
+        errorList.append("ERROR: No meshes in selection or scene.")
+    if errorList == []: 
+        bpy.ops.export_scene.fbx(filepath=os.path.join(FBX_EXPORT_DIR,fileName+".fbx"),
+                            use_selection = options["selection_only"],
+                            add_leaf_bones=False,
+                            object_types={'ARMATURE', 'MESH'})
+        args = ""
+        if options["rewrite"]:
+            args +=" -rewrite"
+        if options["bonenumbers"]:
+            args += " -bonenumbers"
+        if options["flip"]:
+            args += " -flip"
+        if options["bones"]:
+            args += " -bones"
+        if os.path.exists(os.path.join(FBX_EXPORT_DIR,fileName+".fbx")):
+            noesisCommand = "\""+noesisPath + "\" ?cmode " +"\"" + os.path.join(FBX_EXPORT_DIR,fileName+".fbx") + "\" " + "\""+filePath+ "\"" + args
+            print("Running Noesis...")
+            #print(noesisCommand)
+            subprocess.run(noesisCommand,shell=True)
+            try:
+                os.remove(os.path.join(FBX_EXPORT_DIR,fileName+".fbx"))
+            except:
+                warningList.append("WARNING: Failed to delete temporary FBX from TempFBX directory.")
+            print("\033[92m__________________________________\nRE Mesh export finished.\033[0m")
+    else:
+        print("\033[91m__________________________________\nRE Mesh export failed.\033[0m")
+    return (warningList,errorList)
